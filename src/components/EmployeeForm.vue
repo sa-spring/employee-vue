2e5d9a1603675900f2112fa7acecd84cf2c3a613
Chun <caochun@gmail.com>
2020-03-21 00:01:10 +0800

    1-5 chapters

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="employee-form">
+    <form v-on:submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input type="text" v-model="employee.name"/>
+      <label>Employee role</label>
+      <input type="text" v-model="employee.role" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          role: '',
+        },
+      }
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit("add:employee", this.employee)
+        },
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee role</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.role }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-table",
+  props: {
+    employees: Array
+  }
+};
+</script>
+
+<style scoped></style>
